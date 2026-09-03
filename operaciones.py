a8ac3e0a060bad05705cb33fef2cd4479a47ff40
jacob <jacobsoacha@gmail.com>
2026-09-03 10:13:49 -0500

    mi primer commit

---
+++ operaciones.py
@@ -0,0 +1,10 @@
+numero1,numero2 = map(int,input("ingrese dos numeros:").split())
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+
+print ("La suma de los dos numeros es : ", suma)
+print ("La resta de los dos numeros es : ", resta)
+print ("La multiplicacion de los dos numeros es : ", multiplicacion)
+print ("La division de los dos numeros es : ", division)
